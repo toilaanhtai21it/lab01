063700f5794d2d771f591a268f9f7157e4876792
toilaanhtai21it <tainda.21it@vku.udn.vn>
2024-09-18 23:16:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text("I Am Rich",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.orange,
+            ),
+          ),
+          backgroundColor: Colors.black,
+          elevation: 0,
+          centerTitle: true,
+          // leading: Icon(Icons.arrow_back_ios),
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('assets/images/rich.jpg'),
+            //image: NetworkImage('https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
